5ea2da0c34c6d4337510d5ee3fe0aeea6eca9a8d
PScoriae <pierreccesario@gmail.com>
2020-06-30 22:31:53 +0800

    Working version.

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ import random
 # List of colors that the computer can choose from.
 colorList = ['red', 'orange', 'yellow', 'green',
                 'blue', 'indigo', 'violet',]
+numberOfColors = 4
 
 def makeAnswerList(list, len):
-    '''Returns a list of random strings from a list. Uses random.choices.
-    len is the length of the new list.
-    '''
+    '''Returns a list of random strings from a list.'''
     return random.choices(list, k=len)
 
-def makeComparisonList(list):
-    '''Makes a copy of a list for future comparison.'''
+def copyList(list):
+    '''Makes a copy of a list for comparison.'''
     return list[:]
 
 def isMatch(guesses, answer):
-    '''Checks if guesses list is equal to answer list.'''
+    '''Checks if two lists are equal.'''
     return guesses == answer
 
 def getList(list):
@@ -26,40 +25,6 @@ def getList(list):
         else:
             print(string + ', ', end='')
 
-
-# Welcome message and premise.
-print('''
-Welcome to MastermindPy! The objective of the game is to guess 4 colors
-in the correct order that I have randomly chosen from a list of colors.
-Colors can be repeated. Each color that you guess must be entered once.
-You can type 'help' at any time for a list of commands.
-''')
-
-print('Here is the list: ')
-
-getList(colorList)
-
-print("\n\nLet's begin!")
-
-while True:
-    # Number of colors to be chosen for answer list.
-    numberOfColors = 4
-
-    # Number of correct colors in the correct place.
-    corColCorPl = 0
-
-    # Number of correct colors in the wrong place.
-    corColWrongPl = 0
-
-    # Empty list to hold the values of user's guesses.
-    guesses = []
-
-    answer = makeAnswerList(colorList)
-    print(answer)
-    comparisonList = makeComparisonList(answer)
-    startGuessing(guesses, numberOfColors)
-
-
 def startGuessing(emptyList, len):
     '''Takes the input of the user and appends them to the emptyList
     for len amount of times.
@@ -68,17 +33,81 @@ def startGuessing(emptyList, len):
         guess = input('Enter your guess here: ')
         emptyList.append(guess.lower())
 
-# Checks for correct colors in the correct place.
-for colorIndex in range(numberOfColors):
-    if guesses[colorIndex] == answer[colorIndex]:
-        corColCorPl += 1
-        comparisonList.remove(guesses[colorIndex])
+    return None
 
-# Checks for correct colors in the wrong place.
-for colorIndex in range(numberOfColors):
-    if guesses[colorIndex] in comparisonList:
-            corColWrongPl += 1
+def congrats(guessCount):
+    print('Congratulations! You matched all the colors! You won the game!')
+    if guessCount == 1:
+        print('It took you 1 guess to figure out the colours!')
+    else:
+        print('It took you ' + guessCount + ' guesses to figure out the colours!')
 
-print('Here are your results for this attempt.')
-print('Correct colors in the correct place: ' + str(corColCorPl))
-print('Correct colors in the wrong place: ' + str(corColWrongPl))
+def wantRepeat():
+    while True:
+        x = input('Would you like to play again?(Y/n)\n') or 'y'
+        if x.lower() in ['yes', 'y']:
+            return True
+        elif x.lower() in ['no', 'n']:
+            return False
+        else:
+            print("I don't understand. Please type 'y' or 'n'")
+
+def main():
+    answer = makeAnswerList(colorList, numberOfColors)
+    print('Here is the list of colors I will choose from: ')
+    getList(colorList)
+    print("\n\nLet's begin!")
+    while True:
+        guessCount = 0
+        guesses = []
+        guessCount += 1
+        guesses = []
+        print(answer)
+        corColCorPl = 0
+        corColWrongPl = 0
+        startGuessing(guesses, numberOfColors)
+        guessesCopy = copyList(guesses)
+        comparisonList = copyList(answer)
+        print(f'{guesses} guesses list')
+        print(f'{guessesCopy} guess copy list')
+        print(f'{answer} answer list')
+        print(f'{comparisonList} comparison list')
+
+        # Checks for correct colors in the correct place.
+        for colorIndex in range(numberOfColors):
+            if guesses[colorIndex] == answer[colorIndex]:
+                corColCorPl += 1
+                comparisonList.remove(guesses[colorIndex])
+                guessesCopy.remove(guesses[colorIndex])
+        print(f'{comparisonList} comparison list')
+        print(f'{guessesCopy} guesses list')
+
+        # Checks for correct colors in the wrong place.
+        for colorIndex in range(len(guessesCopy)):
+            if guessesCopy[colorIndex] in comparisonList:
+                    corColWrongPl += 1
+
+        print('\nHere are your results for this attempt.')
+        print('Correct colors in the correct place: ' + str(corColCorPl))
+        print('Correct colors in the wrong place: ' + str(corColWrongPl))
+        if isMatch(guesses, answer):
+            congrats(guessCount)
+            print('This is the answer list: ')
+            getList(answer)
+            break
+        else:
+            print('Try again.')
+
+    if wantRepeat():
+        main()
+    else:
+        print('Thanks for playing! Goodbye.')
+
+print('''
+Welcome to MastermindPy! The objective of the game is to guess 4 colors
+in the correct order that I have randomly chosen from a list of colors.
+Colors can be repeated. Each color that you guess must be entered once.
+You can type 'help' at any time for a list of commands.
+''')
+
+main()
